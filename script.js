abd174dcba725b6dc2dd6e2758dfff441397d9fe
davidgjermeni <davidgjermeni@gmail.com>
2026-02-21 17:50:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const questions = [{
+    questions:"How many pyramids are at the Giza plateau? (Including the queens)",
+    answers:[
+      {text: "6 pyramids", correct: false},
+      {text: "9 pyramids", correct: true},
+      {text: "10 pyramids", correct: false},
+      {text: "12 pyramids", correct: false}  
+    ]
+
+},
+
+{
+    questions:"What do Greek celebrate on October 28th?",
+    answers:[
+      {text: "Ohi <όχι> day (to Ottomans)", correct: false},
+      {text: "Independence day 1821 (from Italy)", correct: false},
+      {text: "Independence day 1821 (from Ottomans)", correct: false},
+      {text: "Ohi <όχι> day (to Italy)", correct: true}  
+    ]
+
+},
+
+{
+    questions:"Whats the elevation of Mount Everest?",
+    answers:[
+      {text: "8,529m", correct: false},
+      {text: "8,243m", correct: false},
+      {text: "8,849m", correct: true},
+      {text: "8,950m", correct: false}  
+    ]
+
+},
+
+{
+    questions:"How many planets do we have in our solar system?",
+    answers:[
+      {text: "8 planets", correct: true},
+      {text: "9 planets", correct: false},
+      {text: "6 planets", correct: false},
+      {text: "10 planets", correct: false}  
+    ]
+
+}
+
+]
